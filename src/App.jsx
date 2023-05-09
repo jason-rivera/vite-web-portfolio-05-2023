34576c4c82a9eae363b3230bd84c731a399281fc
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-05-09 14:30:10 -0700

    updated hero section

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,36 @@ function App() {
       <div className={styles.topBanner}>
         <div className={styles.topBannerContent}>
           <div>
-            <div>JASON RIVERA</div>
-            <div>Web Developer</div>
+            <div className={styles.header}>
+              JASON <b>RIVERA</b>
+            </div>
+            <div className={styles.subheader}>Web Developer</div>
           </div>
-          <div>GIT / INSTA / EMAIL</div>
+          <div className={styles.heroIconSection}>
+            <div className={styles.heroIconContainer}>
+              <img
+                className={styles.heroIcon}
+                src='https://www.svgrepo.com/show/512317/github-142.svg'
+              />
+            </div>
+            <div className={styles.heroIconContainer}>
+              <img
+                className={styles.heroIcon}
+                src='https://www.svgrepo.com/show/506517/linkedin.svg'
+              />
+            </div>
+            <div className={styles.heroIconContainer}>
+              <img
+                className={styles.heroIcon}
+                src='https://www.svgrepo.com/show/511917/email-1572.svg'
+              />
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.aboutSection}>
         <div className={styles.aboutContent}>
-          <h1>ABOUT</h1>
+          <div className={styles.sectionTitle}>ABOUT</div>
           <p>
             Hi, I'm Jason. I'm a web developer and UI/UX designer. My primary
             expertise is in front-end development and design, particularly
@@ -28,8 +49,13 @@ function App() {
           </p>
         </div>
       </div>
-      <div className={styles.projectsSection}>Projects section</div>
-      <div className={styles.footer}>Footer</div>
+      <div className={styles.projectsSection}>
+        <div className={styles.sectionTitle}>Projects section</div>
+      </div>
+      <div className={styles.footer}>
+        <div>©2023 Jason Rivera · MIT License</div>
+        <div>GIT / LINKEDIN / EMAIL</div>
+      </div>
     </>
   );
 }
