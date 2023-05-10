2abe17390d1ece8f73cde6c8e3162ebb518701a3
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-05-09 18:25:22 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <HeroSection />
+      {/** About Section */}
       <div className={styles.aboutSection}>
         <div className={styles.aboutContent}>
           <div className={styles.sectionTitle}>ABOUT</div>
@@ -18,18 +19,30 @@ function App() {
                 <img src='https://media.licdn.com/dms/image/C4E03AQHHLgrFAcaZcg/profile-displayphoto-shrink_800_800/0/1609387611539?e=1689206400&v=beta&t=BXXlFy-OXEjnNCcV3x9BCPkrM8Wd1-ACkj0jJULAFgI' />
               </div>
             </div>
-            <p>
-              Hi, I'm Jason. I'm a web developer and UI/UX designer. My primary
-              expertise is in front-end development and design, particularly
-              HTML/CSS/JS and using frameworks like React to build web apps. I
-              also do backend work, tooling, testing, DevOps, mobile, and
-              small-scale game development. I have a bunch of past projects you
-              can check out below. If you want to get in touch, feel free to
-              shoot me a message on LinkedIn or just email me.
-            </p>
+            <div>
+              <p>
+                Hi, I'm Jason. I'm a web developer and UI/UX designer. My
+                primary expertise is in front-end development and design,
+                particularly HTML/CSS/JS and using frameworks like React to
+                build web apps. I also do backend work, tooling, testing,
+                DevOps, mobile, and small-scale game development.
+              </p>
+              <p>
+                I have a bunch of past projects you can check out below. If you
+                want to get in touch, feel free to shoot me a message on{' '}
+                <a
+                  href='https://www.linkedin.com/in/rivera-jason/'
+                  target='_blank'
+                >
+                  LinkedIn
+                </a>{' '}
+                or just <a href='mailto:jp_rivera_@hotmail.com'>email me</a>.
+              </p>
+            </div>
           </div>
         </div>
       </div>
+      {/** Projects Section */}
       <div className={styles.projectsSection}>
         <div className={styles.sectionTitle}>Projects section</div>
         <div className={styles.projectsSectionContent}>
